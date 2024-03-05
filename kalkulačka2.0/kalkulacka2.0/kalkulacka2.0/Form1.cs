3e963f3e55d31ca107edb92ce3c9ea6d498d4d62
FilaVila <144894437+FilaVila@users.noreply.github.com>
2024-03-05 17:58:07 +0100

    kalkulacka 2.0

---
+++ kalkulačka2.0/kalkulacka2.0/kalkulacka2.0/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kalkulacka2._0
+{
+    public partial class Form1 : Form
+    {
+        string number;
+        double result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            number = number + "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            number = number + "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            number = number + "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            number = number + "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            number = number + "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            number = number + "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            number = number + "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            number = number + "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            number = number + "9";
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            number = number + "0";
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonNas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDeleno_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRov_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAns_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
